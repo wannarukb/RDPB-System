03032fda2a81e3bf0c237fd80550f8782e8cb6a1
wbill86 <developer.wwnr@gmail.com>
2017-09-10 19:10:52 +0700

    adding files

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
-  
-    
-    
-    var nextTabID = 1;
-    
-  
-     $('.nextBtn').click(function(){
-        var curStep = $('#form-wizard .active').find('a'),
-            curStepBtn = curStep.attr("href");
-            curStep.removeClass('active');
-        var currentStep = curStepBtn.replace('#tab-','');
-        var currentID = parseInt(currentStep);
-        if(currentID == 3){
-             nextTabID = 1;
-         }
-        else{
-             nextTabID = currentID+1; 
-        }
-       
-        var nextStep = '#tab-' + nextTabID;
-        console.log('nextStep > '+ nextStep);
-         
-         $('#form-wizard a[href="'+nextStep+'"]').tab('show');
-         
-    });
-  
-    
-});
+//$(document).ready(function(){
+//  
+//    
+//    
+//    var nextTabID = 1;
+//    
+//  
+//     $('.nextBtn').click(function(){
+//        var curStep = $('#form-wizard .active').find('a'),
+//            curStepBtn = curStep.attr("href");
+//            curStep.removeClass('active');
+//        var currentStep = curStepBtn.replace('#tab-','');
+//        var currentID = parseInt(currentStep);
+//        if(currentID == 3){
+//             nextTabID = 1;
+//         }
+//        else{
+//             nextTabID = currentID+1; 
+//        }
+//       
+//        var nextStep = '#tab-' + nextTabID;
+//        console.log('nextStep > '+ nextStep);
+//         
+//         $('#form-wizard a[href="'+nextStep+'"]').tab('show');
+//         
+//    });
+//  
+//    
+//});
 
 
 $(function() {
--- js/demo/form-file-upload.js
+++ js/demo/form-file-upload.js
@@ -38,64 +38,64 @@ $(document).ready(function() {
     }
 
 
+//
+//    // DROPZONE.JS WITH BOOTSTRAP'S THEME
+//    // =================================================================
+//    // Require Dropzone
+//    // http://www.dropzonejs.com/
+//    // =================================================================
+//    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
+//    var previewNode = document.querySelector("#dz-template");
+//    previewNode.id = "";
+//    var previewTemplate = previewNode.parentNode.innerHTML;
+//    previewNode.parentNode.removeChild(previewNode);
 
-    // DROPZONE.JS WITH BOOTSTRAP'S THEME
-    // =================================================================
-    // Require Dropzone
-    // http://www.dropzonejs.com/
-    // =================================================================
-    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-    var previewNode = document.querySelector("#dz-template");
-    previewNode.id = "";
-    var previewTemplate = previewNode.parentNode.innerHTML;
-    previewNode.parentNode.removeChild(previewNode);
-
-    var uplodaBtn = $('#dz-upload-btn');
-    var removeBtn = $('#dz-remove-btn');
-    var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
-        url: "/target-url", // Set the url
-        thumbnailWidth: 50,
-        thumbnailHeight: 50,
-        parallelUploads: 20,
-        previewTemplate: previewTemplate,
-        autoQueue: false, // Make sure the files aren't queued until manually added
-        previewsContainer: "#dz-previews", // Define the container to display the previews
-        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
-    });
+//    var uplodaBtn = $('#dz-upload-btn');
+//    var removeBtn = $('.dz-icon');
+//    var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+//        url: "/target-url", // Set the url
+//        thumbnailWidth: 50,
+//        thumbnailHeight: 50,
+//        parallelUploads: 20,
+//        previewTemplate: previewTemplate,
+//        autoQueue: false, // Make sure the files aren't queued until manually added
+//        previewsContainer: "#dz-previews", // Define the container to display the previews
+//        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+//    });
 
 
-    myDropzone.on("addedfile", function(file) {
-        // Hookup the button
-        uplodaBtn.prop('disabled', false);
-        removeBtn.prop('disabled', false);
-    });
-
-    // Update the total progress bar
-    myDropzone.on("totaluploadprogress", function(progress) {
-        $("#dz-total-progress .progress-bar").css({'width' : progress + "%"});
-    });
-
-    myDropzone.on("sending", function(file) {
-        // Show the total progress bar when upload starts
-        document.querySelector("#dz-total-progress").style.opacity = "1";
-    });
-
-    // Hide the total progress bar when nothing's uploading anymore
-    myDropzone.on("queuecomplete", function(progress) {
-        document.querySelector("#dz-total-progress").style.opacity = "0";
-    });
+//    myDropzone.on("addedfile", function(file) {
+//        // Hookup the button
+//        uplodaBtn.prop('disabled', false);
+//        removeBtn.prop('disabled', false);
+//    });
+//
+//    // Update the total progress bar
+//    myDropzone.on("totaluploadprogress", function(progress) {
+//        $("#dz-total-progress .progress-bar").css({'width' : progress + "%"});
+//    });
+//
+//    myDropzone.on("sending", function(file) {
+//        // Show the total progress bar when upload starts
+//        document.querySelector("#dz-total-progress").style.opacity = "1";
+//    });
+//
+//    // Hide the total progress bar when nothing's uploading anymore
+//    myDropzone.on("queuecomplete", function(progress) {
+//        document.querySelector("#dz-total-progress").style.opacity = "0";
+//    });
 
 
     // Setup the buttons for all transfers
-    uplodaBtn.on('click', function() {
-        //Upload all files
-        //myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
-    });
-
-    removeBtn.on('click', function() {
-        myDropzone.removeAllFiles(true);
-        uplodaBtn.prop('disabled', true);
-        removeBtn.prop('disabled', true);
-    });
+//    uplodaBtn.on('click', function() {
+//        //Upload all files
+//        //myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+//    });
+//
+//    removeBtn.on('click', function() {
+//        myDropzone.removeAllFiles(true);
+//        uplodaBtn.prop('disabled', true);
+//        removeBtn.prop('disabled', true);
+//    });
 
 });
